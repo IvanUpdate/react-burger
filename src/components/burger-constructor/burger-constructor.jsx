4a878c3ba408726d90e87e9897cc3826c1c20a33
Ivan <mironovvan99@gmail.com>
2021-12-13 11:13:39 +0300

    fixed problem with open app

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -4,10 +4,13 @@ import { ConstructorElement, DragIcon, CurrencyIcon, Button, LockIcon } from '@y
 import PropTypes from 'prop-types';
 import Modal from "../modal/modal";
 import OrderDetails from '../order-details/order-details';
+import data from '../../utils/data';
 
 export default function BurgerConstructor (props){
 
     const [modal, setModal] = useState(false);
+
+    const img = 'https://code.s3.yandex.net/react/code/bun-02-mobile.png'
 
     const switchModal = () => {
         setModal(!modal)
@@ -16,8 +19,8 @@ export default function BurgerConstructor (props){
     return (
         <>
         <div className={burgerConstructorStyles.main + ' pt-25'}>
-            <div className='mt-4 ml-8' key={props.data[0]._id}>
-                <ConstructorElement text={props.data[0].name} price={props.data[0].price} thumbnail={props.data[0].image} isLocked={true} type='top'  />
+            <div className='mt-4 ml-8'>
+                <ConstructorElement text="Краторная булка N-200i (верх)" price={200} thumbnail={img} isLocked={true} type='top'  />
             </div>
             <div className={burgerConstructorStyles.scrollArea}>
                 {props.data.map((item) => {
@@ -30,7 +33,7 @@ export default function BurgerConstructor (props){
                 })}
             </div>
             <div className='mt-4 ml-8'>
-                <ConstructorElement text={props.data[0].name} price={props.data[0].price} thumbnail={props.data[0].image} isLocked={true} type='bottom' key={props.data[0]._id} />
+                <ConstructorElement text="Краторная булка N-200i (верх)" price={200} thumbnail={img} isLocked={true} type='bottom'  />
             </div>
             <div className={burgerConstructorStyles.total + ' mt-10'}>
                 <span className={burgerConstructorStyles.price + ' mr-10'}>610<CurrencyIcon type="primary" /></span>
